a7d6000c2f4926a416d66e19d262237bffdb5c65
Shamil Musema Reshad <ShamilM@ZAMZAMBANK.LOCAL>
2025-02-04 15:29:23 +0300

    Api Development

--- Fast_API/database.py
+++ Fast_API/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://etech:etech@localhost/eth_medical_db"
+DATABASE_URL = "postgresql://postgres:sh36am67@localhost/my_database"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
